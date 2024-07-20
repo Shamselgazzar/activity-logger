ec8cdc170747ee931bca3284272e82fd61afbf21
Shamseldin Elgazzar <selgazzar@sumerge.com>
2024-07-20 15:40:15 +0300

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
+import React from "react";
 import { useState } from "react";
-
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/table";
+
 
 
 export default function Home() {
