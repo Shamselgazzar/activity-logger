a23694c5cd6858ae0097356eefb19becec2095f8
Shamseldin Elgazzar <selgazzar@sumerge.com>
2024-07-24 01:40:19 +0300

    export functionality

---
+++ src/utils/exportUtils.ts
@@ -0,0 +1,50 @@
+import { DetailedEvent } from "../models/DetailedEvent";
+import { EventsResponse,  } from "../models/EventsResponse";
+
+// todo make the headers pretty
+// todo fix metadata
+
+
+export const handleExport = (filteredData: EventsResponse) => {
+  const json2csv = (json : any) => {
+    const fields = [
+      "id", "object", "location", "occurredAt",
+      "actor.id", "actor.name", "actor.email", "actor.group", 
+      "action.id", "action.name", "action.object",
+      "target.id", "target.name", "target.email", "target.group",
+    ];
+    const replacer = (key: any, value: any) => value === null ? '' : value;
+    let csv = json.map((row : any) => fields.map(fieldName => {
+      const value = fieldName.split('.').reduce((acc, key) => acc && acc[key], row);
+      return JSON.stringify(value, replacer);
+    }).join(','));
+    csv.unshift(fields.join(','));
+    csv = csv.join('\r\n');
+    return csv;
+  };
+
+  const csv = json2csv(filteredData.events);
+
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+
+  // download
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.setAttribute("href", url);
+  link.setAttribute("download", getFileName());
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+
+export const getFileName = () => {
+  const now = new Date();
+  const year = now.getFullYear().toString().padStart(4, '0');
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const day = now.getDate().toString().padStart(2, '0');
+  const hour = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+
+  return `event-logs-${year}-${month}-${day}-T${hour}${minutes}.csv`;
+}
