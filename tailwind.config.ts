f17aeca971874dbe0cfc373786a86ae6b1560206
Shamseldin Elgazzar <selgazzar@sumerge.com>
2024-07-24 17:24:17 +0300

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx,css}",
     "./node_modules/@nextui-org/theme/dist/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
