03a300cc53a8617ba86d0eef7ece99da2df7c129
Shamseldin Elgazzar <selgazzar@sumerge.com>
2024-07-25 22:55:38 +0300

    fix responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,44 +111,52 @@ export default function Home() {
   // UI View
   return (
     <div className="p-6 rounded-lg border-gray-300">
+      
       <h1 className="text-2xl font-bold my-4 text-gray-600 mx-auto text-center">
         Activity Logger
       </h1>
+
       <div id="main-container" className="max-w-5xl mx-auto border rounded-xl border-gray-300 bg-gray-100">
-        <div id="search-container" className="bg-gray-100 px-2 mx-5 mt-5 mb-2 rounded-xl flex items-center border border-gray-200">
-          <input
-            type="text"
-            title="name, email, action, or group..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Search name, email, action, or group..."
-            className="text-sm font-semibold bg-gray-100 text-gray-800 px-2 py-1 border-b-0 border-gray-300 flex-grow focus:outline-none"
-          />
-          <div className="border-l border-gray-300 h-10 mx-2"></div>
-          <button
-            onClick={handleFilter}
-            className="px-3 py-1 bg-gray-100 text-gray-800 text-xs rounded flex items-center justify-center hover:scale-105"
-          >
-            <Image src='/filter.svg' alt='filter icon' width={20} height={20} className="mr-1" />
-            FILTER
-          </button>
-          <div className="border-l border-gray-300 h-10 mx-2"></div>
-          <button
-            onClick={onExportClick}
-            className="px-3 py-1 bg-gray-100 text-gray-800 text-xs rounded flex items-center justify-center hover:scale-105"
-          >
-            <Image src='/export.svg' alt='export icon' width={20} height={20} className="mr-1" />
-            EXPORT
-          </button>
-          <div className="border-l border-gray-300 h-10 mx-2"></div>
-          <button
-            onClick={toggleLiveView}
-            className="px-3 py-1 bg-gray-100 text-xs text-gray-800 rounded flex items-center justify-center hover:scale-105"
-          >
-            <Image src='/live.svg' alt='live icon' width={15} height={15} className="mr-1" />
-            LIVE
-          </button>
+
+      <div className="bg-gray-100 px-2 mx-5 mt-5 mb-2 rounded-xl flex flex-wrap items-center border border-gray-200">
+          <div className="w-full sm:flex-grow sm:w-auto">
+            <input
+              type="text"
+              title="name, email, action, or group..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search name, email, action, or group..."
+              className="text-sm font-semibold bg-gray-100 text-gray-800 px-2 py-1 border-b-0 border-gray-300 w-full sm:flex-grow focus:outline-none"
+            />
+          </div>
+          <div className="border-l border-gray-300 h-10 mx-2 hidden sm:block"></div>
+          <div className="w-full flex sm:w-auto sm:flex-wrap">
+            <button
+              onClick={handleFilter}
+              className="px-3 py-1 bg-gray-100 text-gray-800 text-xs rounded flex items-center justify-center hover:scale-105 w-full sm:w-auto"
+            >
+              <Image src='/filter.svg' alt='filter icon' width={20} height={20} className="mr-1" />
+              FILTER
+            </button>
+            <div className="border-l border-gray-300 h-10 mx-2 hidden sm:block"></div>
+            <button
+              onClick={onExportClick}
+              className="px-3 py-1 bg-gray-100 text-gray-800 text-xs rounded flex items-center justify-center hover:scale-105 w-full sm:w-auto"
+            >
+              <Image src='/export.svg' alt='export icon' width={20} height={20} className="mr-1" />
+              EXPORT
+            </button>
+            <div className="border-l border-gray-300 h-10 mx-2 hidden sm:block"></div>
+            <button
+              onClick={toggleLiveView}
+              className="px-3 py-1 bg-gray-100 text-xs text-gray-800 rounded flex items-center justify-center hover:scale-105 w-full sm:w-auto"
+            >
+              <Image src='/live.svg' alt='live icon' width={15} height={15} className="mr-1" />
+              LIVE
+            </button>
+          </div>
         </div>
+
 
         <div id="table-container">
           <Table aria-label="events-table" selectionMode="single" 
@@ -184,6 +192,7 @@ export default function Home() {
             </TableBody>
           </Table>
         </div>
+
         <div id="load-more-container" className="bg-gray-100 px-2 p-2 flex justify-between items-center text-center rounded-b-xl relative -mt-4">
           {!isLoading ? (
             <button
@@ -198,6 +207,7 @@ export default function Home() {
             <div className="text-gray-600 mx-auto">Loading...</div>
           )}
         </div>
+
       </div>
 
       <EventDetailsModal
